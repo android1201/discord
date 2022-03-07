ff62037070d61c1423326bcb1e8c0404d1b6ce38
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-08 05:12:00 +0530

    update Embed.js

--- src/classes/Embed.js
+++ src/classes/Embed.js
@@ -12,22 +12,95 @@ module.exports = class Embed extends MessageEmbed {
 		};
 		this.setColor(this.opts.color);
 	};
-	Color(d) {
+	AddAuthor(d) {
+		this.setAuthor(d);
+		return this;
+	};
+	AddColor(d) {
 		if (!d) {
 			d = this.opts.color;
-		}
+		};
 		if (d) {
 			let finded = color_list.find((x) => x.name.toLowerCase() === d.toLowerCase());
 			if (typeof finded === "undefined") {
 				d = d || this.opts.color;
 			} else {
 				d = finded.hex;
-			}
-		}
+			};
+		};
 		this.setColor(d);
 		return this;
 	};
-	getColors() {
+	AddDescription(d) {
+		this.setDescription(d);
+		return this;
+	};
+	AddField(d) {
+		this.addField(d);
+		return this;
+	};
+	AddFields(d) {
+		this.addFields(d);
+		return this;
+	};
+	AdFields(d) {
+		this.setFields(d);
+		return this;
+	};
+	AddFooter(d) {
+		this.setFooter(d);
+		return this;
+	};
+	AddImage(d) {
+		this.setImage(d);
+		return this;
+	};
+	AddThumbnail(d) {
+		this.setThumbnail(d);
+		return this;
+	};
+	AddTimestamp(d) {
+		this.setTimestamp(d);
+		return this;
+	};
+	AddTitle(d) {
+		this.setTitle(d);
+		return this;
+	};
+	AddUrl(d) {
+		this.setURL(d);
+		return this;
+	};
+	get CreatedAt() {
+		return this.timestamp ? new Date(this.timestamp) : null;
+	};
+	Equals(embed) {
+		return (
+			this.type === embed.type &&
+			this.author?.name === embed.author?.name &&
+			this.author?.url === embed.author?.url &&
+			this.author?.iconURL === (embed.author?.iconURL ?? embed.author?.icon_url) &&
+			this.color === embed.color &&
+			this.title === embed.title &&
+			this.description === embed.description &&
+			this.url === embed.url &&
+			this.timestamp === embed.timestamp &&
+			this.fields.length === embed.fields.length &&
+			this.fields.every((field, i) => this._fieldEquals(field, embed.fields[i])) &&
+			this.footer?.text === embed.footer?.text &&
+			this.footer?.iconURL === (embed.footer?.iconURL ?? embed.footer?.icon_url) &&
+			this.image?.url === embed.image?.url &&
+			this.thumbnail?.url === embed.thumbnail?.url &&
+			this.video?.url === embed.video?.url &&
+			this.provider?.name === embed.provider?.name &&
+			this.provider?.url === embed.provider?.url
+		);
+	};
+	FieldEquals(d) {
+		this._fieldEquals(d);
+		return this;
+	};
+	GetColors() {
 		let data = color_list.reduce((map, obj, i, a) => {
 			map[obj.name] = obj.hex;
 			return map;
@@ -38,6 +111,9 @@ module.exports = class Embed extends MessageEmbed {
 		});
 		this.opts.list.join(',');
 		return this.opts.list;
+	};
+	get HexColor() {
+		return this.color ? `#${this.color.toString(16).padStart(6, '0')}` : null;
 	};
 	Json() {
 		return {
@@ -60,5 +136,20 @@ module.exports = class Embed extends MessageEmbed {
 				icon_url: this.footer.iconURL,
 			},
 		};
+	};
+	get Length() {
+		return (
+			(this.title?.length ?? 0) +
+			(this.description?.length ?? 0) +
+			(this.fields.length >= 1 ?
+				this.fields.reduce((prev, curr) => prev + curr.name.length + curr.value.length, 0) :
+				0) +
+			(this.footer?.text.length ?? 0) +
+			(this.author?.name.length ?? 0)
+		);
+	};
+	RemoveFields(d) {
+		this.spliceFields(d);
+		return this;
 	};
 };
