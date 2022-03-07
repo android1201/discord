f221d7170279698e2c9b7bd51adaadf5f169150b
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-08 02:32:46 +0530

    update Embed.js

--- src/classes/Embed.js
+++ src/classes/Embed.js
@@ -1,7 +1,26 @@
-const {
-	MessageEmbed
-} = require('discord.js');
-
+const color_list = require('../data/color_list.js').database,
+	{
+		MessageEmbed
+	} = require("discord.js");
 module.exports = class Embed extends MessageEmbed {
-	constructer(opts = {}, data = {}) {}
+	constructor(opts = {}, data = {}) {
+		super(data);
+		var colorList = color_list;
+		this.list = [];
+		let {
+			color = null
+		} = opts;
+	}
+	getColors() {
+		let data = color_list.reduce((map, obj, i, a) => {
+			map[obj.name] = obj.hex;
+			return map;
+		}, {});
+		let newMap = new Map(Object.entries(data));
+		newMap.forEach((value, key) => {
+			this.list.push(key);
+		});
+		this.list.join(',');
+		return this.list;
+	}
 };
