4bfbd52cc1d92d9aa9202f7228722835b291d9be
Durgesh Kumar <89626036+devdk2004@users.noreply.github.com>
2022-03-08 05:53:37 +0530

    update Embed.js

--- src/testings/Embed.js
+++ src/testings/Embed.js
@@ -1,1 +1,14 @@
-
+const {
+	Embed
+} = require('../module.js');
+const embed = new Embed();
+embed.AddColor('')
+	.AddAuthor('author', 'https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/2x/external-url-essentials-icongeek26-outline-gradient-icongeek26.png')
+	.AddDescription('AddDescription')
+	.AddFooter('AddFooter', 'https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/2x/external-url-essentials-icongeek26-outline-gradient-icongeek26.png')
+	.AddImage('https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/2x/external-url-essentials-icongeek26-outline-gradient-icongeek26.png')
+	.AddThumbnail('https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/2x/external-url-essentials-icongeek26-outline-gradient-icongeek26.png')
+	.AddTimestamp()
+	.AddTitle('Title')
+	.AddUrl('https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/2x/external-url-essentials-icongeek26-outline-gradient-icongeek26.png');
+console.log(embed.Json())
